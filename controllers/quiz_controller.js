428ddb7e7a1798c1cbe954ec20ee1c0f6d3052c9
jmartinezvalles <jmartinezvalles@outlook.com>
2015-07-13 17:44:53 +0200

    Busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,70 @@
-// GET /quizes/question
-
-exports.question = function(req, res) {
-  res.render('quizes/question', {pregunta: 'Capital de Italia'});
+﻿/**
+ * @return {string}
+ */
+function GetAnswer(req) {
+    if (req.query.respuesta === req.quiz.respuesta) {
+        return 'Correcto';
+    }
+    return 'Incorrecto';
 }
 
-// GET /quizes/answer
+/**
+ * @return {string}
+ */
+function ReplaceSpacesByPercentage(search) {
+    search = search.split(" ");
+    search = search.join('%');
+    return search;
+}
+
+/**
+ * @return {string}
+ */
+function AddDelimiters(search) {
+    return '%' + search + '%';
+}
+function GetQueryFormatted(req) {
+    var search = req.query.search;
+    search = ReplaceSpacesByPercentage(search);
+    search = AddDelimiters(search);
+    return search;
+}
+
+var models = require('../models/models.js');
+
+exports.load = function(req, res, next, quizId){
+    models.Quiz.find(quizId).then(
+        function(quiz){
+            if(quiz){
+                req.quiz = quiz;
+                next();
+            }
+            else{
+                next(new Error('No existe quizId='+ quizId));
+            }
+        }
+    ).catch(function(error){ next(error)});
+};
+
+exports.index = function(req, res) {
+    var query = {};
+
+    if (req.query.search) {
+        query = {where: ["lower(pregunta) like lower(?)", GetQueryFormatted(req)], order: 'pregunta ASC'}
+    }
+
+    models.Quiz.findAll(query).then(
+        function (quizes) {
+        res.render('quizes/index', {quizes: quizes});
+        }
+    ).catch(function(error){ next(error)});
+};
+
+exports.show = function(req, res) {
+    res.render('quizes/show', {quiz: req.quiz});
+};
+
 
 exports.answer = function(req, res) {
-  if (req.query.respuesta === 'Roma') {
-    res.render('quizes/answer', {respuesta: 'Correcto'});
-  } else {
-    res.render('quizes/answer', {respuesta: 'Incorrecto'});
-  }
-}
+    res.render('quizes/answer', { quiz:req.quiz, respuesta: GetAnswer(req) });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-var quizController = require('../controllers/quiz_controller')
-var creditsController = require('../controllers/credits_controller');
+var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.param('quizId', quizController.load);
 
-router.get('/author', creditsController.credits);
+router.get('/author', function (req, res) {
+  res.render('author', { title: 'Quiz', author: 'MrBro' });
+});
+
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
